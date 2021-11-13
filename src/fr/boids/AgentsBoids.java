6a856ac238870146240bf1b9ef73767317687249
Haroun Al Mounayar <Haroun.Al-Mounayar@grenoble-inp.org>
2021-11-13 13:23:14 -0500

    Coding style

--- src/fr/boids/AgentsBoids.java
+++ src/fr/boids/AgentsBoids.java
@@ -1,45 +1,88 @@
+/**
+ * This module is about simulating boids.
+ * It consists of two classes:
+ *  <ul>
+ *   <li>AgentBoids : the part responsible for doing calculation
+ *                    for 1 boids </li>
+ *   <li>AgentsBoids : the part responsible of doing calculations
+ *                    for a list of boids.</li>
+ *   <li>AgentsSim : for drawing </li>
+ *  </ul>
+ *
+ */
+
 package fr.boids;
 
 import java.util.Iterator;
 import java.util.LinkedList;
-import fr.boids.AgentBoids;
 import fr.glob.MyVector;
 import java.util.Random;
-import java.util.Vector;
 import java.awt.Color;
 
+/** AgentsBoids.
+ */
 public class AgentsBoids {
+
+    /** The list of boids. */
     private LinkedList<AgentBoids> agentsList;
+
+    /** The copy of list of boids. */
     private LinkedList<AgentBoids> copy;
-    private int w,h;
-    // {Color.RED, Color.BLUE, Color.GREEN, Color.ORANGE,
-    //     Color.CYAN, Color.MAGENTA, Color.YELLOW, Color.PINK};
+
+    /** The width. */
+    private int w;
+
+    /** The height. */
+    private int h;
+
+    /** The color of boids. */
     public static Color[] colors = {Color.RED};
 
-    public AgentsBoids(int nbToAdd, int w, int h){
+    /** The constructor of the many Boids.
+     * @param nbToAdd : the number of boids
+     * @param w : the width
+     * @param h : the height
+     * */
+    public AgentsBoids(final int nbToAdd, final int w, final int h) {
         agentsList = new LinkedList<AgentBoids>();
         copy = new LinkedList<AgentBoids>();
-        this.w = w; this.h = h;
+        this.w = w;
+        this.h = h;
         addRandomAgents(nbToAdd);
     }
 
-    public LinkedList<AgentBoids> getAgents(){
+    /** Get the list of boids.
+     * @return the list.
+     * */
+    public LinkedList<AgentBoids> getAgents() {
         return this.agentsList;
     }
+
+    /** Get the width.
+     * @return the width.
+     * */
     public int getWidth() {
         return this.w;
     }
+
+    /** Get the height.
+     * @return the height.
+     * */
     public int getHeight() {
         return this.h;
     }
 
-    public void reInit(){
-        if(agentsList.size() != copy.size()) 
+    /** Restitute the initial list saved in the copy. */
+    public void reInit() {
+        if (agentsList.size() != copy.size()) {
             throw new RuntimeException("reInit : Listes de taille différentes!");
+        }
         Iterator<AgentBoids> itL = agentsList.iterator();
         Iterator<AgentBoids> itC = copy.iterator();
-        AgentBoids a,c;
-        while(itL.hasNext() && itC.hasNext()){
+        AgentBoids a;
+        AgentBoids c;
+
+        while (itL.hasNext() && itC.hasNext()) {
             a = itL.next();
             c = itC.next();
             a.getPosition().setXY(c.getPosition());
@@ -48,33 +91,67 @@ public class AgentsBoids {
         }
     }
 
-    public void addRandomAgents(int nbToAdd){
+
+    /** Add random agent boids to the list.
+     * @param nbToAdd : the number of boids to add. */
+    public void addRandomAgents(final int nbToAdd) {
         Random r = new Random();
         Color c;
-        int x, y,dx, dy, maxSpeed=(int)AgentBoids.vitMax, radius = 5;
+        int x;
+        int y;
+        int dx;
+        int dy;
+        int maxSpeed = (int) AgentBoids.vitMax;
+        int radius = 5;
         //int radiusMin = 10, radiusMax = 15;
-        for (int i = 0; i < nbToAdd; i++){
+        for (int i = 0; i < nbToAdd; i++) {
             //radius = r.nextInt(radiusMax-radiusMin) + radiusMin;
-            x = r.nextInt(w-radius*2) + radius; // Intervalle [radius; w-radius]
-            y = r.nextInt(h-radius*2) + radius;
-            dx =  r.nextInt(maxSpeed) - maxSpeed/2; //soustraction pour centrer l'intervalle en 0 
-            dy = r.nextInt(maxSpeed) - maxSpeed/2;
-            c = colors[i%colors.length];
-            addAgent(new MyVector(x,y),new MyVector(dx,dy), radius, c);
+            // Intervalle [radius; w-radius]
+            x = r.nextInt(w - radius * 2) + radius;
+            y = r.nextInt(h - radius * 2) + radius;
+            //soustraction pour centrer l'intervalle en 0
+            dx = r.nextInt(maxSpeed) - maxSpeed / 2;
+            dy = r.nextInt(maxSpeed) - maxSpeed / 2;
+            c = colors[i % colors.length];
+            addAgent(new MyVector(x, y), new MyVector(dx, dy), radius, c);
         }
     }
 
-    public void addAgent(MyVector position, int rayon, Color c){
+
+    /** Add one boids agent to the list.
+     * @param position : the position of the boids
+     * @param rayon : the radius of the boids
+     * @param c : the color of the boids
+     * */
+    public void addAgent(final MyVector position,
+                         final int rayon,
+                         final Color c) {
         agentsList.add(new AgentBoids(position, rayon, c));
-        copy.add(new AgentBoids(new MyVector(position), rayon, c)); //Copie de position necessaire
+        //Copie de position necessaire
+        copy.add(new AgentBoids(new MyVector(position), rayon, c));
     }
 
-    public void addAgent(MyVector position, MyVector vitesse, int rayon, Color c){
-        agentsList.add(new AgentBoids(position,vitesse, rayon, c));
-        copy.add(new AgentBoids(new MyVector(position), new MyVector(vitesse), rayon, c)); //Copie de position necessaire
+    /** Add one boids agent to the list but with speed.
+     * @param position : the position of the boids
+     * @param rayon : the radius of the boids
+     * @param c : the color of the boids
+     * @param vitesse : the speed of the boids
+     * */
+    public void addAgent(final MyVector position,
+                         final MyVector vitesse,
+                         final int rayon,
+                         final Color c) {
+        agentsList.add(new AgentBoids(position, vitesse, rayon, c));
+        copy.add(new AgentBoids(new MyVector(position),
+                                new MyVector(vitesse),
+                                rayon,
+                                c)); //Copie de position necessaire
     }
-    
-    public void update(){
-        for (AgentBoids a : agentsList) a.update(w, h, agentsList);
+
+    /** Update the list of boids Agents. */
+    public void update() {
+        for (AgentBoids a : agentsList) {
+            a.update(w, h, agentsList);
+        }
     }
 }
