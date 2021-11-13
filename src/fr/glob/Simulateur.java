c623fb6cd71ee94988a3ddd64bbf5f4d32a0c9c3
Haroun Al Mounayar <Haroun.Al-Mounayar@grenoble-inp.org>
2021-11-13 17:51:50 -0500

    Coding style simulator

--- src/fr/glob/Simulateur.java
+++ src/fr/glob/Simulateur.java
@@ -1,3 +1,5 @@
+/** This is our abstract class for simulation. */
+
 package fr.glob;
 
 import gui.Simulable;
@@ -8,12 +10,29 @@ import java.awt.Color;
 
 import fr.glob.event.EventManager;
 
-public abstract class Simulateur implements Simulable{
-    protected int w,h;
+/** Simulateur. */
+public abstract class Simulateur implements Simulable {
+
+    /** The width. */
+    protected int w;
+
+    /** The height. */
+    protected int h;
+
+    /** The screen where we want to draw. */
     protected GUISimulator win;
+
+    /** Our event manager. */
     protected EventManager em;
 
-    public Simulateur(int w, int h, GUISimulator win) {
+    /** The constructor of abstract simulator.
+     *  @param w : the width
+     *  @param h : the height
+     *  @param win : where we want to Draw.
+     * */
+    public Simulateur(final int w,
+                      final int h,
+                      final GUISimulator win) {
         this.w = w;
         this.h = h;
         this.win = win;
@@ -21,31 +40,37 @@ public abstract class Simulateur implements Simulable{
     }
 
 
-    //Dessine les bords du "panel", où les balls peuvent se déplacer
-    protected void drawEdge(Color c){
-        Rectangle r = new Rectangle(w/2,0, c, c, w, 1);
+    /** Draw the panel where agents and ball can move.
+     *  @param c : the color of the edge.
+     */
+    protected void drawEdge(final Color c) {
+        Rectangle r = new Rectangle(w / 2, 0, c, c, w, 1);
         win.addGraphicalElement(r);
-        r = new Rectangle(0,h/2, c, c, 1, h);
+        r = new Rectangle(0, h / 2, c, c, 1, h);
         win.addGraphicalElement(r);
-        r = new Rectangle(w/2,h-1, c, c, w, 1);
+        r = new Rectangle(w / 2, h - 1, c, c, w, 1);
         win.addGraphicalElement(r);
-        r = new Rectangle(w-1,h/2, c, c, 1, h);
+        r = new Rectangle(w - 1, h / 2, c, c, 1, h);
         win.addGraphicalElement(r);
-        //Partie perso
-        // Oval o = new Oval(w/2, h/2, Color.yellow, Color.DARK_GRAY, 20);
-        // addGraphicalElement(o);
     }
 
+    /** The next method. */
     public abstract void next();
+
+    /** The restart method. */
     public abstract void restart();
 
+
+    /** The toString method.
+     * @return the string representing the screen.
+     */
     @Override
     public String toString() {
-        return "{" +
-            " w='" + w + "'" +
-            ", h='" + h + "'" +
-            "}";
+        return "{"
+            + " w='" + w + "'"
+            + ", h='" + h + "'"
+            + "}";
     }
 
-    
+
 }
