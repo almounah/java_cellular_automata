c5dac4fe095ee1bee9f1fd437cacaa7a69f2cebe
Haroun Al Mounayar <Haroun.Al-Mounayar@grenoble-inp.org>
2021-11-13 18:37:39 -0500

    fix style error in vector

--- src/fr/glob/MyVector.java
+++ src/fr/glob/MyVector.java
@@ -4,76 +4,80 @@ public class MyVector {
     public double x;
     public double y;  
 
-    public MyVector(double x, double y) {
+    public MyVector(final double x, final double y) {
         this.x = x;
         this.y = y;
     }
 
-    public MyVector(MyVector v) {
+    public MyVector(final MyVector v) {
         this.x = v.x;
         this.y = v.y;
     }
 
-
-    public static MyVector sub(MyVector a, MyVector b){
+    public static MyVector sub(final MyVector a, final MyVector b) {
         return new MyVector(a.x - b.x, a.y - b.y);
     }
 
-    public static double produitSclaire(MyVector a, MyVector b){
-        return a.x*b.x + a.y*b.y;
+    public static double produitSclaire(final MyVector a, final MyVector b) {
+        return a.x * b.x + a.y * b.y;
     }
 
-    public static double angle(MyVector a, MyVector b){
-        double value = produitSclaire(a, b) / (a.masse()*b.masse());
-        if(value<-1 || value>1) System.out.println("ERREUR: non-logical cos!");
-        return java.lang.Math.acos(value)* (180.0/java.lang.Math.PI);
+    public static double angle(final MyVector a, final MyVector b) {
+        double value = produitSclaire(a, b) / (a.masse() * b.masse());
+        if (value < -1 || value > 1) {
+            System.out.println("ERREUR: non-logical cos!");
+        }
+        return java.lang.Math.acos(value) * (180.0 / java.lang.Math.PI);
     }
 
-    public double dst(MyVector a){
-        MyVector d = sub(this,a);
+    public final double dst(final MyVector a) {
+        MyVector d = sub(this, a);
         return d.masse();
     }
 
-    public void setXY(MyVector a){
+    public final void setXY(final MyVector a) {
         this.x = a.x;
         this.y = a.y;
     }
 
-    public void add(MyVector v) {
+    public final void add(final MyVector v) {
         y = y + v.y;
         x = x + v.x;
     }
 
-    public void div(double n) {
-        if(n==0.0) return;
+    public final void div(final double n) {
+        if (n == 0.0) {
+            return;
+        }
         x = x / n;
         y = y / n;
     }
 
-    public void mult(double n) {
+    public final void mult(final double n) {
         x = x * n;
         y = y * n;
     }
 
-    public double masse(){
-        return java.lang.Math.sqrt(x*x + y*y);
+    public final double masse() {
+        return java.lang.Math.sqrt(x * x + y * y);
     }
 
-    public void normalize(){
+    public final void normalize() {
         double m = masse();
-        if(m!=0) div(m);
+        if (m != 0) {
+            div(m);
+        }
     }
 
-    public void limit(double max){
-        if(masse() > max){
+    public final void limit(final double max) {
+        if (masse() > max) {
             normalize();
             mult(max);
         }
     }
 
-
     @Override
-    public String toString() { // 2 chiffres après la virgule
+    public final String toString() { // 2 chiffres après la virgule
         String fin = "{x=";
         String s = String.valueOf(x)+"000";
         int ip = s.indexOf('.');
