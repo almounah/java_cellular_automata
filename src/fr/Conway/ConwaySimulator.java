503bff5485ba9b133c389b7927a29e9823630d2f
Haroun Al Mounayar <Haroun.Al-Mounayar@grenoble-inp.org>
2021-11-13 06:10:47 -0500

    Coding style 4

--- src/fr/Conway/ConwaySimulator.java
+++ src/fr/Conway/ConwaySimulator.java
@@ -1,54 +1,100 @@
+/**
+ * This module is about simulating Conway game of life.
+ * It consists of two classes:
+ *  <ul>
+ *   <li>ConwayGrid : the part responsible for doing calculation</li>
+ *   <li>ConwaySimulator : the part responsible of drawing</li>
+ *  </ul>
+ *
+ * Conway Game of life will later be extended to Schelling segregation
+ * and to the Immigration game.
+ *
+ * @author Haroun Al Mounayar
+ */
 package fr.Conway;
 
-import fr.glob.*;
+import fr.glob.Simulateur;
 import gui.GUISimulator;
 import gui.Rectangle;
 import java.awt.Color;
 import java.util.HashMap;
 import java.util.ArrayList;
 
+/**
+ * ConwaySimulator.
+ */
 public class ConwaySimulator extends Simulateur {
-    public int size_of_square, rows; //taille d'un petit carre et le nombre de ligne
-    public int init_alive;
-    public int half_square; 
-    private Color list_Colors[]; 
-    private ConwayGrid grid; 
 
-    public ConwaySimulator(int size_of_square, int rows, int init_alive, GUISimulator win) { 
+    /** The size of a square. */
+    public int sizeOfSquare;
+
+    /** The number of rows in the square Grid. */
+    public int rows;
+
+    /** The number of initialy alive cells. */
+    public int initAlive;
+
+    /** The half of the sizeOfSquare. */
+    private int halfSquare;
+
+    /** The list of the colors we want to use. */
+    private Color[] listColors;
+
+    /** The grid of type ConwayGrid. */
+    private ConwayGrid grid;
+
+    /** The constructor of the game of life.
+     *  @param sizeOfSquare the size of a single square
+     *  @param rows the number of rows
+     *  @param initAlive the number of initialy Alive cells
+     *  @param win the GUISimulator we want to draw on
+     */
+    public ConwaySimulator(final int sizeOfSquare,
+                           final int rows,
+                           final int initAlive,
+                           final GUISimulator win) {
         this(
-            size_of_square, 
-            rows, init_alive, 
-            new ConwayGrid(rows, rows, init_alive), 
+            sizeOfSquare,
+            rows, initAlive,
+            new ConwayGrid(rows, rows, initAlive),
             new Color[]{Color.WHITE, Color.BLACK},
-            win); // we chose a squared grid always 
+            win);
     }
 
-
+ 
+     /** The constructor for all grid types.
+     *  @param sizeOfSquare the size of a single square
+     *  @param rows the number of rows
+     *  @param initAlive the number of initialy Alive cells
+     *  @param grid the ConwayGrid we want to draw
+     *  @param win the GUISimulator we want to draw on
+     *  @param listColors the list of the color
+     */
     protected ConwaySimulator(
-            int size_of_square, 
-            int rows, 
-            int init_alive, 
-            ConwayGrid grid, 
-            Color list_Colors[],
-            GUISimulator win) {
-        super(size_of_square*rows, size_of_square*rows, win);
-        this.size_of_square = size_of_square;
+            final int sizeOfSquare,
+            final int rows,
+            final int initAlive,
+            final ConwayGrid grid,
+            final Color[] listColors,
+            final GUISimulator win) {
+        super(sizeOfSquare * rows, sizeOfSquare * rows, win);
+        this.sizeOfSquare = sizeOfSquare;
         this.rows = rows;
-        this.init_alive = init_alive;
+        this.initAlive = initAlive;
         this.grid = grid;
-        this.list_Colors = list_Colors;
+        this.listColors = listColors;
         this.win = win;
-        half_square = size_of_square/2;
+        halfSquare = sizeOfSquare / 2;
         this.grid.initialize();
     }
 
     public void draw_grid_line() {
         Rectangle r;
-        int size = size_of_square*rows;
-        for (int i = 0; i <= size; i+=size_of_square) {
-            r = new Rectangle(i, size/2, Color.GRAY, Color.WHITE, 1, size);
+        int size = sizeOfSquare * rows;
+        for (int i = 0; i <= size; i += sizeOfSquare) {
+            r = new Rectangle(i, size / 2, Color.GRAY, Color.WHITE, 1, size);
             win.addGraphicalElement(r);
-            r = new Rectangle(size/2, i, Color.GRAY, Color.WHITE, size, 1); 
+            r = new Rectangle(size / 2, i, Color.GRAY, Color.WHITE, size, 1); 
             win.addGraphicalElement(r);
         } 
     }
@@ -57,9 +103,9 @@ public class ConwaySimulator extends Simulateur {
     public void draw_cube(int x, int y, Color c) {
         /*x and y are the coordinate of the top left*/    
         Rectangle r;
-        int cube_size = size_of_square-4;
-        int x_center = x + half_square;
-        int y_center = y + half_square;
+        int cube_size = sizeOfSquare-4;
+        int x_center = x + halfSquare;
+        int y_center = y + halfSquare;
         r = new Rectangle(x_center, y_center, c, c, cube_size, cube_size);
         win.addGraphicalElement(r);
     }
@@ -67,7 +113,7 @@ public class ConwaySimulator extends Simulateur {
     public void draw_grid()  {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < rows; j++) {
-                draw_cube(j*size_of_square, i*size_of_square, list_Colors[grid.grid[j][i]]); 
+                draw_cube(j*sizeOfSquare, i*sizeOfSquare, listColors[grid.grid[j][i]]); 
             }
         }
     }
@@ -75,7 +121,7 @@ public class ConwaySimulator extends Simulateur {
 
     @Override
     public String toString() {
-        return ("The board of size " + size_of_square*rows);
+        return ("The board of size " + sizeOfSquare*rows);
     }
 
     @Override
