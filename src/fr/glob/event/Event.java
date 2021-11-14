4fd60ab430c35a683a05c4d212fde500f28b5a42
Haroun Al Mounayar <Haroun.Al-Mounayar@grenoble-inp.org>
2021-11-14 07:45:48 -0500

    Style + doc for event

--- src/fr/glob/event/Event.java
+++ src/fr/glob/event/Event.java
@@ -1,29 +1,61 @@
+/**This Module is used to make Events.
+ * It contains many classes :
+ *  <ul>
+ *      <li> Event an abstract </li>
+ *      <li> Event manager that has many events </li>
+ *      <li> Event Message for strings. </li>
+ *      <li> Event Message to test events later with strings. </li>
+ *           like eventBalls, eventgrid ... </li>
+ *  </ul>
+ * */
 package fr.glob.event;
 
 import java.lang.Comparable;
 
+/** Event.
+ */
 public abstract class Event implements Comparable<Event> {
+
+    /** The date when the Event will be played. */
     private long dateToPlay;
+
+    /** The event manager used to add the event to. */
     private EventManager em;
 
+    /** The constructor.
+     * @param dateToPlay : the date when the event will be played
+     * @param eventManager : the eventmanager that manage the event
+     * */
     public Event(long dateToPlay, EventManager eventManager) {
         this.dateToPlay = dateToPlay;
         this.em = eventManager;
     }
 
+    /** Get the date when the event will be played.
+     *  @return the date.
+     * */
     public long getDateToPlay() {
         return this.dateToPlay;
     }
 
+    /** Get the event manager that manage the Event.
+     *  @return the event manager.
+     * */
     public EventManager getEventManager() {
         return this.em;
     }
 
+    /** We override the method compareTo.
+     * It compares two events.
+     * @param other : the other event.
+     * @return 1 if the event is executed later that other
+     *         0 if not.
+     * */
     @Override
     public int compareTo(Event other) {
         // On retourne -1 si la date de this est plus petite que other,
         // this aura alors une priorité plus grande
-        if(this.getDateToPlay() > other.getDateToPlay()) {
+        if (this.getDateToPlay() > other.getDateToPlay()) {
             return 1;
         }
         if (this.getDateToPlay() < other.getDateToPlay()) {
@@ -32,8 +64,12 @@ public abstract class Event implements Comparable<Event> {
         return 0;
     }
 
+    /** The execute method of an event. */
     public abstract void execute();
 
+    /** Return the string representing the event.
+     *  @return the string.
+     * */
     @Override
     public String toString() {
         return "Event at date " + dateToPlay;
--- src/fr/glob/event/EventManager.java
+++ src/fr/glob/event/EventManager.java
@@ -1,33 +1,59 @@
+/**This Module is used to make Events.
+ * It contains many classes :
+ *  <ul>
+ *      <li> Event an abstract </li>
+ *      <li> Event manager that has many events </li>
+ *      <li> Many other classes that extends The abstract Event
+ *      <li> Many other classes that extends The abstract Event
+ *           like eventBalls, eventgrid ... </li>
+ *  </ul>
+ * */
 package fr.glob.event;
 
 import java.util.Iterator;
 import java.util.PriorityQueue;
 
+/** EventManager.
+ */
 public class EventManager {
+    /** The current time. */
     private long currentDate;
+
+    /** The queue representing all events. */
     private PriorityQueue<Event> eventQueue;
+
+    /** The copy of the queue representing all initial events. */
     private PriorityQueue<Event> initialQueue;
 
-
+    /** The constructor of the Event Manager. */
     public EventManager() {
         this.currentDate = 0;
         this.eventQueue = new PriorityQueue<Event>();
         this.initialQueue = null;
     }
 
-    public void addEvent(Event e){
+    /** Add the event to the eventManager.
+     * @param e : the event to add.
+     * */
+    public void addEvent(Event e) {
         boolean b = eventQueue.add(e);
-        if(!b) {
+        if (!b) {
             System.err.println("EVENT ERROR: L'evenement ne s'est pas ajouté comme il faut!");
         }
     }
 
-    // Reste t'il des Event?
+    /** Tell us if the list is not empty.
+     *  @return false. if the list is empty
+     *          true if not.
+     * */
     public boolean isMoreEvent() {
         return !eventQueue.isEmpty();
     }
 
-    // Reste t'il des Event à executer pour la date courante?
+    /** The isfinished method.
+     * @return True if we still have event to execute
+     *         at our date. False if not.
+     * */
     public boolean isFinished() {
         Event e = eventQueue.peek(); // Ne retire pas l'élément de la queue
         if (e == null) {
@@ -39,43 +65,46 @@ public class EventManager {
         return true;
     }
 
+    /** Execute the next event. */
     public void next() {
         currentDate++;
-        //System.out.println("New date! -> "+currentDate);
         while (!isFinished()) {
             Event e = eventQueue.poll();
             e.execute();
         }
     }
 
-    //Sauvegarde l'êtat actuel comme état initiale de la queue (necessaire pour restart)
+    /** Save the current state in the initialQueue.
+     *  usefull for the restart.
+     * */
     public void setInitialStatus() {
-        if(initialQueue!=null) {
+        if (initialQueue != null) {
             System.err.println("ERROR : initialQueue est déjà définie!!!");
             return;
         }
         initialQueue = new PriorityQueue<Event>();
         Iterator<Event> it = eventQueue.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             Event e = it.next();
             initialQueue.add(e);
         }
     }
 
+    /** Restart method when debut is pressed. */
     public void restart() {
-        if(initialQueue==null) {
-            System.err.println("ERROR : l'état initiale de la queue d'Event "+
-                "n'a jamais été défini! Il faut utiliser 'setInitialStatus()'");
+        if (initialQueue == null) {
+            System.err.println("ERROR : l'état initiale de la queue d'Event "
+                + "n'a jamais été défini! Il faut utiliser 'setInitialStatus()'");
             return;
         }
         currentDate = 0;
         eventQueue.clear();
         Iterator<Event> it = initialQueue.iterator();
-        while(it.hasNext()){
+        while (it.hasNext()) {
             Event e = it.next();
             eventQueue.add(e);
         }
-        
+
     }
 
 }
